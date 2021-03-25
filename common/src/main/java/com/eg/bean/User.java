eb49b21806b707f43c7c4fa5fd1e2a89ba4228d7
makewheels <makewheels>
2021-03-25 23:30:54 +0800

    init

---
+++ common/src/main/java/com/eg/bean/User.java
@@ -0,0 +1,14 @@
+package com.eg.bean;
+
+import lombok.Data;
+
+/**
+ * @Author makewheels
+ * @Time 2021.03.25 23:19:55
+ */
+@Data
+public class User {
+    private String userId;
+    private String userName;
+
+}
---
+++ user/src/main/java/com/eg/user/UserApplication.java
@@ -0,0 +1,8 @@
+package com.eg.user;
+
+/**
+ * @Author makewheels
+ * @Time 2021.03.25 23:24:26
+ */
+public class UserApplication {
+}
