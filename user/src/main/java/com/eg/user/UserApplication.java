bb46a520341f04854864fedccdc08a13b894a475
makewheels <makewheels>
2021-03-26 23:12:24 +0800

    init

--- user/src/main/java/com/eg/user/UserApplication.java
+++ user/src/main/java/com/eg/user/UserApplication.java
@@ -1,8 +1,11 @@
 package com.eg.user;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Author makewheels
  * @Time 2021.03.25 23:24:26
  */
+@SpringBootApplication
 public class UserApplication {
 }
