3547d011f5c47e9333cd32e3d105e8223c93c210
makewheels <makewheels>
2021-03-28 00:37:22 +0800

    ok

--- user/src/main/java/com/eg/testspringcloudalibaba/user/UserApplication.java
+++ user/src/main/java/com/eg/testspringcloudalibaba/user/UserApplication.java
@@ -1,18 +1,13 @@
-package com.eg.user;
+package com.eg.testspringcloudalibaba.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 
-/**
- * @Author makewheels
- * @Time 2021.03.25 23:24:26
- */
 @EnableDiscoveryClient
 @SpringBootApplication
 public class UserApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
     }
